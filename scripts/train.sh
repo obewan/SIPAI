af6548eadfdffe7d0fb9a731259f20737670d46b
obewan <damien.balima@free.fr>
2024-04-05 22:54:46 +0200

    fix: epochs without improvement

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#script to test the TrainingMonitored on a release, for local testing
+# cript to test the TrainingMonitored on a release, for local testing
 cd ../build
-./sipai --en test.json --tf test/images-test1.csv --isx 64 --isy 64 --hsx 64 --hsy 64 --osx 64 --osy 64 --hl 1 --haf ReLU --oaf ReLU -m TrainingMonitored
+./sipai --en test.json --tf test/images-test1.csv --isx 64 --isy 64 --hsx 64 --hsy 64 --osx 64 --osy 64 --hl 1 --haf ReLU --oaf ReLU --lr 0.005 -m TrainingMonitored
