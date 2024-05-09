5fa85e1cd03f5e9b2624c57ab560272f1162bcb6
obewan <damien.balima@free.fr>
2024-05-09 17:54:42 +0200

    test: add tests

--- test/doctests/TestRunnerTrainingMonitorVisitor.cpp
+++ test/doctests/TestRunnerTrainingMonitorVisitor.cpp
@@ -26,6 +26,34 @@ TEST_CASE("Testing RunnerTrainingMonitoredVisitor") {
     manager.app_params.training_data_folder = "";
     CHECK_THROWS_AS(visitor.visit(), RunnerVisitorException);
 
+    manager.network.reset();
+
+    auto &ap = manager.app_params;
+    ap.training_data_file = "";
+    ap.training_data_folder = "../data/images/target/";
+    ap.max_epochs = 2;
+    ap.run_mode = ERunMode::TrainingMonitored;
+    ap.network_to_export = "tempNetwork.json";
+    ap.network_to_import = "";
+    ap.enable_vulkan = false;
+    ap.random_loading = true;
+    ap.verbose = true;
+    ap.verbose_debug = true;
+    std::string network_csv = "tempNetwork.csv";
+
+    auto &np = manager.network_params;
+    np.input_size_x = 2;
+    np.input_size_y = 2;
+    np.hidden_size_x = 3;
+    np.hidden_size_y = 2;
+    np.output_size_x = 3;
+    np.output_size_y = 3;
+    np.hiddens_count = 1;
+
+    manager.createOrImportNetwork();
+    CHECK_NOTHROW(visitor.visit());
+
+    manager.network.reset();
     TrainingDataFactory::getInstance().clear();
   }
 }
