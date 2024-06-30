0df69bd784dfb687859b9a55076651191c76264a
obewan <damien.balima@free.fr>
2024-06-30 16:41:47 +0200

    fix: creating graphic pipeline

--- libs/libsipai/include/VulkanCommon.h
+++ libs/libsipai/include/VulkanCommon.h
@@ -59,6 +59,28 @@ const std::map<EBuffer, std::string, std::less<>> buffer_map{
 struct Vertex {
   float pos[2];
   float color[3];
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription = {};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 2>
+  getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(Vertex, pos);
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(Vertex, color);
+    return attributeDescriptions;
+  }
 };
 
 struct GLSLParameters {
--- libs/libsipai/src/VulkanBuilder.cpp
+++ libs/libsipai/src/VulkanBuilder.cpp
@@ -49,14 +49,14 @@ VulkanBuilder &VulkanBuilder::build() {
   _updateDescriptorSets();
   _createShaderModules();
   _createPipelineLayout();
+  _createSurface();
+  _createSwapChain();
+  _createRenderPass();
   _createShaderPipelines();
   _createCommandPool();
   _allocateCommandBuffers();
   _createFence();
-  _createSurface();
-  _createSwapChain();
   _createImageViews();
-  _createRenderPass();
   _createFramebuffers();
 
   return *this;
@@ -704,13 +704,94 @@ void VulkanBuilder::_createShaderPipelines() {
     }
   }
 
+  // get vertex infos
+  VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+  vertexInputInfo.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+
+  auto bindingDescription = Vertex::getBindingDescription();
+  auto attributeDescriptions = Vertex::getAttributeDescriptions();
+
+  vertexInputInfo.vertexBindingDescriptionCount = 1;
+  vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+  vertexInputInfo.vertexAttributeDescriptionCount =
+      static_cast<uint32_t>(attributeDescriptions.size());
+  vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+
+  VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+  inputAssembly.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+  inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+  inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+  VkViewport viewport = {};
+  viewport.x = 0.0f;
+  viewport.y = 0.0f;
+  viewport.width = (float)vulkan_->swapChainExtent.width;
+  viewport.height = (float)vulkan_->swapChainExtent.height;
+  viewport.minDepth = 0.0f;
+  viewport.maxDepth = 1.0f;
+
+  VkRect2D scissor = {};
+  scissor.offset = {0, 0};
+  scissor.extent = vulkan_->swapChainExtent;
+
+  VkPipelineViewportStateCreateInfo viewportState = {};
+  viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+  viewportState.viewportCount = 1;
+  viewportState.pViewports = &viewport;
+  viewportState.scissorCount = 1;
+  viewportState.pScissors = &scissor;
+
+  VkPipelineRasterizationStateCreateInfo rasterizer = {};
+  rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+  rasterizer.depthClampEnable = VK_FALSE;
+  rasterizer.rasterizerDiscardEnable = VK_FALSE;
+  rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+  rasterizer.lineWidth = 1.0f;
+  rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+  rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+  rasterizer.depthBiasEnable = VK_FALSE;
+
+  VkPipelineMultisampleStateCreateInfo multisampling = {};
+  multisampling.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+  multisampling.sampleShadingEnable = VK_FALSE;
+  multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+  VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+  colorBlendAttachment.colorWriteMask =
+      VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+      VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+  colorBlendAttachment.blendEnable = VK_FALSE;
+
+  VkPipelineColorBlendStateCreateInfo colorBlending = {};
+  colorBlending.sType =
+      VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+  colorBlending.logicOpEnable = VK_FALSE;
+  colorBlending.logicOp = VK_LOGIC_OP_COPY;
+  colorBlending.attachmentCount = 1;
+  colorBlending.pAttachments = &colorBlendAttachment;
+  colorBlending.blendConstants[0] = 0.0f;
+  colorBlending.blendConstants[1] = 0.0f;
+  colorBlending.blendConstants[2] = 0.0f;
+  colorBlending.blendConstants[3] = 0.0f;
+
   // create graphic pipelines
   vulkan_->infoGraphics.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
   vulkan_->infoGraphics.stageCount = (uint32_t)shaderGraphicsStages.size();
   vulkan_->infoGraphics.pStages = shaderGraphicsStages.data();
+  vulkan_->infoGraphics.pVertexInputState = &vertexInputInfo;
+  vulkan_->infoGraphics.pInputAssemblyState = &inputAssembly;
+  vulkan_->infoGraphics.pViewportState = &viewportState;
+  vulkan_->infoGraphics.pRasterizationState = &rasterizer;
+  vulkan_->infoGraphics.pMultisampleState = &multisampling;
+  vulkan_->infoGraphics.pColorBlendState = &colorBlending;
   vulkan_->infoGraphics.layout = vulkan_->pipelineLayout;
   vulkan_->infoGraphics.basePipelineHandle = VK_NULL_HANDLE;
   vulkan_->infoGraphics.basePipelineIndex = 0;
+  vulkan_->infoGraphics.renderPass = vulkan_->renderPass;
+  vulkan_->infoGraphics.subpass = 0;
   if (vkCreateGraphicsPipelines(vulkan_->logicalDevice, VK_NULL_HANDLE, 1,
                                 &vulkan_->infoGraphics, nullptr,
                                 &vulkan_->pipelineGraphic) != VK_SUCCESS) {
